5f43c238d76a40b051be5b1841f8aa0828f801f2
panelhenil-oss <panelhenil@gmail.com>
2026-09-11 05:02:54 +0200

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@
    Example:
    const NEW_LINK = "https://example.com/latest-script";
    ============================================================ */
-const NEW_LINK = "https://link-center.net/1258318/HdAcKh80Bx2n";
+const NEW_LINK = "https://direct-link.net/1239053/dhzEm8jnnnj7";
 /* ============================================================
    DO NOT EDIT BELOW THIS LINE
    ============================================================ */
